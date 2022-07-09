6c04a20f7af21cec73cb56a6c3519ec1febda884
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-07-09 17:44:56 -0400

    completed

--- roll.py
+++ roll.py
@@ -4,4 +4,62 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+import math
+import random
+
+class Dice:
+    def __init__(self, num_sides=6):
+        self.num_sides = num_sides
+
+    def roll(self):
+        dice1 = random.randint(1, self.num_sides)
+        sum = dice1
+        print(f"Dice 1: {dice1}")
+        print(f"Total: {sum}")
+
+    def rolltwice(self):
+        dice1 = random.randint(1, self.num_sides)
+        print(f"Dice 1: {dice1}")
+        dice2 = random.randint(1, self.num_sides)
+        print(f"Dice 2: {dice2}")
+        sum = dice1 + dice2
+        print(f"Total: {sum}")
+    def rollthrice(self):
+        dice1 = random.randint(1, self.num_sides)
+        print(f"Dice 1: {dice1}")
+        dice2 = random.randint(1, self.num_sides)
+        print(f"Dice 2: {dice2}")
+        dice3 = random.randint(1, self.num_sides)
+        print(f"Dice 3: {dice3}")
+        sum = dice1 + dice2 + dice3
+        print(f"Total: {sum}")
+
+
+
+
+
+
+
+
+die = Dice()
+
+"""Dice game"""
+diceOn = True
+while diceOn == True:
+    print("How many time would you like to roll a dice? (1-3) press 0 to quit")
+    diceToss = int(input(">> "))
+    if diceToss == 1:
+        die.roll()
+    elif diceToss == 2:
+        die.rolltwice()
+    elif diceToss == 3:
+        die.rollthrice()
+    elif diceToss == 0:
+        diceOn = False
+    else:
+        print("not enough dice")
+print("Thanks for playing")
+
+
+
 
